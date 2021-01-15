b22bea199bebcd80c90b447f31f8da8a766d042d
Daniela Bergman <daniela.bergman@hotmail.com>
2021-01-15 22:28:43 +0100

    added date format to threads

--- frontend/src/components/Thread.js
+++ frontend/src/components/Thread.js
@@ -3,11 +3,29 @@ import "../index.css";
 
 const Thread = (props) => {
   const [creator, setCreator] = useState([]);
+  const [date, setDate] = useState("");
 
   useEffect(()=>{
     getCreator();
+    formatDate();
     console.log(props);
   },[])
+
+  const formatDate = () => {
+    let date_ob = new Date(props.thread.date);
+
+    let date = ("0" + date_ob.getDate()).slice(-2);
+
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+    let year = date_ob.getFullYear();
+
+    let hours = date_ob.getHours();
+
+    let minutes = date_ob.getMinutes();
+
+    setDate(year + "-" + month + "-" + date + " " + hours + ":" + minutes);
+  };
 
 
   const getCreator = async() =>{
@@ -23,7 +41,7 @@ const Thread = (props) => {
     <div className="thread-div">
       <p className="thread-title">{props.thread.title}</p>
       <div className="thread-bottom-row">
-        <p className="thread-date">{props.thread.date}</p>
+        <p className="thread-date">{date}</p>
         <p className="thread-creator">{creator.username}</p>
       </div>
     </div>
