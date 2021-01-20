dcb654e67bb4a66564082421715e11e719bbfee4
Daniela Bergman <daniela.bergman@hotmail.com>
2021-01-20 08:37:47 +0100

    added index.js to run build version with pm2

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Switch, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { ForumContext } from "../context/ForumContextProvider";
 import "../index.css";
 
