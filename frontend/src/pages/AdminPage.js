78305267583ed4e0d5d203681d5efe08d6ca148c
Daniela Bergman <daniela.bergman@hotmail.com>
2021-01-21 17:00:42 +0100

    Restricted access to /admin page

--- frontend/src/pages/AdminPage.js
+++ frontend/src/pages/AdminPage.js
@@ -4,16 +4,49 @@ import "../index.css";
 import Card  from "react-bootstrap/Card";
 import Accordion from "react-bootstrap/Accordion";
 import User from "../components/User";
+import { Alert } from "reactstrap";
 
 const AdminPage = (props) => {
   const forumContext= useContext(ForumContext);
   const [users, setUsers] = useState([]);
+  const [allowed, setAllowed] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState([]);
 
   useEffect(()=>{
     forumContext.fetchAllUsers();
+    
+    getLoggedInUser();
   },[]);
   
 
+  const getLoggedInUser = async()=> {
+
+    setLoggedInUser(await forumContext.fetchLoggedInUser());
+
+  }
+
+  useEffect(()=>{
+
+    console.log(loggedInUser);
+    checkUser();
+  },[loggedInUser])
+
+  const checkUser =()=>{
+    if(loggedInUser!== null){
+      if(loggedInUser.userRole ==="admin"){
+        setAllowed(true);
+      }
+      else{
+        setAllowed(false);
+      }
+    }else{
+      setAllowed(false);
+    }
+  }
+
+  useEffect(()=>{
+    console.log(allowed);
+  },[allowed])
   // const fetchAllUsers = async ()=>{
   //   let response = await fetch("/api/users", {
   //     method: "GET",
@@ -32,6 +65,7 @@ const AdminPage = (props) => {
 
     return (
       <div className="accordion-div">
+        {allowed ?
         <Accordion>
           <Card>
             <Accordion.Toggle
@@ -62,6 +96,10 @@ const AdminPage = (props) => {
             </Accordion.Collapse>
           </Card>
         </Accordion>
+        :
+        <Alert className="admin-warning" color="warning">
+          Stopp och belägg! Denna sidan är till för administratörer!
+        </Alert>        }
       </div>
     );
 }
